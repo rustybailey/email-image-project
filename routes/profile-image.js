914488d457e79e697dd03db633a3215ef9768d50
Rusty Bailey <rustylbailey@gmail.com>
2015-09-28 16:48:42 -0400

    Cache image response.

--- routes/profile-image.js
+++ routes/profile-image.js
@@ -1,19 +1,36 @@
+var Promise = require('bluebird');
 var express = require('express');
 var router = express.Router();
 var rp = require('request-promise');
+var NodeCache = require('node-cache');
+var myCache = new NodeCache();
+Promise.promisifyAll(myCache);
 
 // TODO: Potentially split logic into separate file for testability?
 // maybe do unit tests and e2e tests?
 // TODO: Ping gravatar first, then fall back to google image search
+// TODO: Validate email
+// TODO: Create API to flush cache and get keys/stats
 
-/* GET users listing. */
+/* GET images from Google based on email. */
 router.get('/:email', function(req, res) {
   var encodedEmail = req.params.email;
   // TODO: Put these two items in a config somewhere
   var googleAPIKey = 'AIzaSyC1G5tI8em5WkSk6RyVwqV5zGLv7e1-hfc';
   var cxCode = '010249180093990001910:krzjvvbgvne';
 
-  rp('https://www.googleapis.com/customsearch/v1?key=' + googleAPIKey + '&cx=' + cxCode + '&q=' + encodedEmail + '&searchType=image')
+  myCache.getAsync('email_' + encodedEmail)
+    .then(function(result) {
+      if (result) {
+        return result;
+      } else {
+        return rp('https://www.googleapis.com/customsearch/v1?key=' + googleAPIKey + '&cx=' + cxCode + '&q=' + encodedEmail + '&searchType=image')
+          .then(function(result) {
+            myCache.setAsync('email_' + encodedEmail, result);
+            return result;
+          });
+      }
+    })
     .then(function(result) {
       var data = JSON.parse(result);
 
@@ -23,3 +40,4 @@ router.get('/:email', function(req, res) {
 });
 
 module.exports = router;
+
