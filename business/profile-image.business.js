e7e1acb13b9b4acfbfb429783037f93c0ac5a3fd
Rusty Bailey <rustylbailey@gmail.com>
2015-09-30 15:35:59 -0400

    Fallback to thumbnail if link contains 'x-raw-image'

--- business/profile-image.business.js
+++ business/profile-image.business.js
@@ -26,12 +26,19 @@ ProfileImageBusiness.prototype.getEmailImage = function(email) {
     .then(function(result) {
       var data = JSON.parse(result);
       var totalResults = parseInt(data.searchInformation.totalResults);
+      var link;
 
       if (!totalResults) {
         return null;
       }
 
-      return data.items[0].link;
+      link = data.items[0].link;
+      // Fallback to thumbnail if link contains 'x-raw-image'
+      if (link.indexOf('x-raw-image') > -1) {
+        link = data.items[0].image.thumbnailLink;
+      }
+
+      return link;
     });
 
 
