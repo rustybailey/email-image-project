f0201089d03bf341b377337660fa974c94f200ff
Rusty Bailey <rustylbailey@gmail.com>
2015-10-01 10:46:05 -0400

    Add error messaging for invalid form and no data returned.

--- public/controllers/email-image.controller.js
+++ public/controllers/email-image.controller.js
@@ -1,6 +1,10 @@
 module.exports = angular.module('emailImageProject')
   .controller('EmailImageController', function($scope, emailImageService) {
     $scope.getImage = function() {
+      if (!$scope.form.$valid) {
+        return false;
+      }
+
       emailImageService.getEmailImage($scope.email)
         .then(function(result) {
           $scope.emailImage = result.data.data;
